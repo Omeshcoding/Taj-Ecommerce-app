4db0cd4b8f428999880323efe1e72d340c932300
umesh <umesharma.us11@gmail.com>
2022-07-30 12:25:55 +0530

    broken our homepage to smaller components, finished styling our menu-items

--- src/components/menu-items/MenuItems.jsx
+++ src/components/menu-items/MenuItems.jsx
@@ -3,9 +3,12 @@ import './menu-items.scss'
 
 const MenuItems = ({title , imageUrl , size}) => {
   return (
-    <div style={{
+    <div className={`${size} menu-item`}>
+      <div className='background-image'
+         style={{
       backgroundImage : `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
+    }}
+    />  
                   <div className='content'>
         <h1 className='title'> {title} </h1>
                       <span className='subtitle'>SHOP NOW</span>
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,30 +4,8 @@ import Directory from '../../components/directory/directory';
 const Homepage = () => {
   return (
       <div className='homepage'>
-        <Directory/>
-              {/* <MenuItems
-                  product='HATS'
-                  shopnow ='SHOP NOW'
-              />
-              <MenuItems
-                  product='JACKETS'
-                  shopnow ='SHOP NOW'
-              />
-              <MenuItems
-                  product='SNEAKERS'
-                  shopnow ='SHOP NOW'
-              />
-              <MenuItems
-                  product='WOMEN'
-                  shopnow ='SHOP NOW'
-              />
-              <MenuItems
-                  product='MENS'
-                  shopnow ='SHOP NOW'
-              /> */}
-      
-
-    </div>
+          <Directory />
+      </div>
   )
 }
 
