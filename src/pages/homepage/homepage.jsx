59ba117dcbae2776bbb811cdac9b91a5a741db4b
umesh <umesharma.us11@gmail.com>
2022-07-30 11:04:35 +0530

    creating menuItems component and styling it

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../components/directory/directory';
+const Homepage = () => {
+  return (
+      <div className='homepage'>
+        <Directory/>
+              {/* <MenuItems
+                  product='HATS'
+                  shopnow ='SHOP NOW'
+              />
+              <MenuItems
+                  product='JACKETS'
+                  shopnow ='SHOP NOW'
+              />
+              <MenuItems
+                  product='SNEAKERS'
+                  shopnow ='SHOP NOW'
+              />
+              <MenuItems
+                  product='WOMEN'
+                  shopnow ='SHOP NOW'
+              />
+              <MenuItems
+                  product='MENS'
+                  shopnow ='SHOP NOW'
+              /> */}
+      
+
+    </div>
+  )
+}
+
+export default Homepage;
